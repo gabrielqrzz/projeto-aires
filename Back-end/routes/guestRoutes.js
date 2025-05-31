3692dad57b251508c2ab6fe9f2f684903c7ff758
kaudy05 <kauysantosferreira109@gmail.com>
2025-05-31 20:15:18 -0300

    Parte do login

--- Back-end/routes/guestRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const guestController = require('../controllers/guestController');
-
-router.get('/', guestController.getGuests);
-router.post('/', guestController.addGuest);
-router.delete('/:id', guestController.deleteGuest);
-
-module.exports = router;
