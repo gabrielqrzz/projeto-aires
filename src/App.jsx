5a940e151024466311fb869298874848e98f0e67
Gabriel <gabrielqueiroz2510@gmail.com>
2025-06-01 16:01:15 -0300

    feat: Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react"
+import InputWithLabel from "./components/InputWithLabel"
 import Sidebar from "./components/Sidebar"
 import "./index.css"
 
 function App() {
+  const [email, setEmail] = useState("")
   return (
     <div className="flex">
       <Sidebar />
+      <div className="">
+        <InputWithLabel
+          label="Email"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite seu email"
+        />
+      </div>
     </div>
   )
 }
