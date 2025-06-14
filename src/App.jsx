1a971f1faaf5423ade080ca9916c6530cd30452d
guiylz <guilucca618@gmail.com>
2025-06-14 15:58:36 -0300

    Tela Cadastro Colaboradores

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import GuestRegistration from "./components/CadastroConvidados"
 
 import "./index.css"
 import HomePage from "./components/HomePage"
+import CadastroColaborador from "./components/CadastroColaboradores"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/cadastroConvidados" element={<GuestRegistration />} />
+        <Route path="/colaboradores" element={<CadastroColaborador />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </>
---
+++ src/components/CadastroColaboradores.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react"
+import InputWithLabel from "./InputWithLabel"
+import { X } from "react-feather"
+import Sidebar from "./Sidebar"
+
+const CadastroColaborador = () => {
+  const [formData, setFormData] = useState({
+    nome: "",
+    nascimento: "",
+    cpf: "",
+    email: "",
+    empresa: "",
+    cargo: "",
+    setor: "",
+    setorDepartamento: "",
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Dados cadastrados:", formData)
+
+    //FUNÇÃO QUE VAI SER USADA PARA ENVIAR OS DADOS PARA API OU LIMPAR OS FORMULARIOS
+  }
+
+  return (
+    <div className="h-screen flex">
+      <Sidebar />
+      <div className=" flex flex-col items-center justify-start bg-white p-6 mt-32 flex-1">
+        {/* Topo */}
+        <div className="flex items-center justify-between w-full max-w-3xl mb-4">
+          <button>
+            <X className="text-brand-red" size={28} />
+          </button>
+          <h1 className="text-xl font-semibold text-brand-red text-center flex-1 -ml-7">
+            Cadastro de Colaboradores
+          </h1>
+        </div>
+
+        {/* Formulário */}
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-3xl bg-white rounded-lg"
+        >
+          <InputWithLabel
+            label="Nome Completo"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            placeholder="Digite seu nome"
+          />
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="flex-1">
+              <InputWithLabel
+                label="Data de Nascimento"
+                name="nascimento"
+                type="date"
+                value={formData.nascimento}
+                onChange={handleChange}
+                placeholder="dd/mm/aaaa"
+              />
+            </div>
+            <div className="flex-1">
+              <InputWithLabel
+                label="CPF (não obrigatório)"
+                name="cpf"
+                value={formData.cpf}
+                onChange={handleChange}
+                placeholder="000.000.000-00"
+              />
+            </div>
+          </div>
+          <InputWithLabel
+            label="E-mail"
+            name="email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Digite seu e-mail"
+          />
+          <div className="flex flex-col mb-4">
+            <label className="mb-1 text-sm font-semibold text-brand-red">
+              Empresa/Instituição
+            </label>
+            <select
+              name="empresa"
+              value={formData.empresa}
+              onChange={handleChange}
+              className="px-2 py-2 border border-brand-red rounded-lg"
+            >
+              <option value="">Selecione</option>
+              <option value="Empresa A">Empresa A</option>
+              <option value="Empresa B">Empresa B</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col mb-6">
+            <label className="mb-1 text-sm font-semibold text-brand-red">
+              Setor/Departamento
+            </label>
+            <select
+              name="setorDepartamento"
+              value={formData.setorDepartamento}
+              onChange={handleChange}
+              className="px-2 py-2 border border-brand-red rounded-lg"
+            >
+              <option value="">Selecione</option>
+              <option value="Setor1">Setor 1</option>
+              <option value="Setor2">Setor 2</option>
+              <option value="Departamento1">Departamento 1</option>
+            </select>
+          </div>
+          {/* Botão de cadastrar */}
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-brand-red text-white font-semibold px-10 py-2 rounded-full"
+            >
+              Cadastrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default CadastroColaborador
